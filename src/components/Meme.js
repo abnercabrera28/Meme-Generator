592a625391592944e071459b1a7a23f36db2f7dd
Abner <abnercabrera28@gmail.com>
2022-06-22 19:46:12 -0400

    add form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,6 +3,11 @@ import React from "react";
 const Meme = () => {
     return (
         <div>
+            <form>
+                <input />
+                <input />
+                <button>Submit</button>
+            </form>
 
         </div>
     )
