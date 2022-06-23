ccc2c8746c5da62faa77377eb83f3a94a25b73d9
Abner <abnercabrera28@gmail.com>
2022-06-22 20:09:18 -0400

    add class names and placeholders to form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,10 +3,20 @@ import React from "react";
 const Meme = () => {
     return (
         <div>
-            <form>
-                <input />
-                <input />
-                <button>Submit</button>
+            <form className="form">
+                <input 
+                    placeholder="Top text" 
+                    className="form-input"   
+                />
+                <input 
+                    placeholder="Bottom text"
+                    className="form-input"
+                />
+                <button
+                    className="form-button"
+                >
+                    Submit
+                </button>
             </form>
 
         </div>
