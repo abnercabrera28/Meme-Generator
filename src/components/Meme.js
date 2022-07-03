8c5ab7cf65bc62ff71b57245426136cc887e8462
Abner <abnercabrera28@gmail.com>
2022-07-03 14:50:25 -0400

    create useState hook that sets allMemes into an array, changed useEffect hook to setAllMemes into the array, changed handleSubmit to pick a random image onClick

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,21 +2,29 @@ import React, { useEffect, useState } from "react";
 
 const Meme = () => {
 
-    const [memeImage, setMemeImage] = useState("")
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = useState([])
 
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
-            .then(data => console.log(data.data.memes))
+            .then(data => setAllMemes(data.data.memes))
     }, [])
-
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        // const memesArray = 
-        // const randomNumber = Math.floor(Math.random() * memes.length)
-        // setMemeImage(memes[randomNumber].url)
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
 
@@ -40,7 +48,7 @@ const Meme = () => {
                 </button>
             </form>
             <div className="meme">
-                <img src={memeImage} className="meme-image" />
+                <img src={meme.randomImage} className="meme-image" />
                 <h2 className="meme-top-text"></h2>
                 <h2 className="meme-bottom-text"></h2>
             </div>
